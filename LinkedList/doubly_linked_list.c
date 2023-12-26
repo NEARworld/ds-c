9f29eaf4bcb2c57c8debb4e479e0cc16374108c4
nearworld <artemismars2@outlook.com>
2023-12-26 21:00:41 +0900

    feat: create node function

--- LinkedList/doubly_linked_list.c
+++ LinkedList/doubly_linked_list.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 typedef struct tagNode {
@@ -6,6 +7,18 @@ typedef struct tagNode {
     struct tagNode* NextNode;
 } Node;
 
+Node* CreateNode(int Data) {
+    Node* NewNode = (Node*)malloc(sizeof(Node));
+    NewNode->Data = Data;
+    NewNode->NextNode = NULL;
+    NewNode->PrevNode = NULL;
+
+    return NewNode;    
+}
+
 int main() {
-    
+    Node* List = NULL;
+    Node* MyNode = CreateNode(10);
+
+    printf("%d\n", MyNode->Data);
 }
