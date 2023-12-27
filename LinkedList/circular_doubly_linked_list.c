255de80778ad5930af6b8f9f5550039e92dad255
nearworld <artemismars2@outlook.com>
2023-12-27 12:23:47 +0900

    feat: Circular Doubly Linked List

---
+++ LinkedList/circular_doubly_linked_list.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct tagNode {
+    int Data;
+    struct tagNode* PrevNode;
+    struct tagNode* NextNode;
+} Node;
+
+void AppendNode(Node** Head, Node* NewNode) {
+    if (*Head == NULL) {
+        *Head = NewNode;
+        return;
+    }
+
+    Node* Tail = *Head;
+
+    while(Tail->NextNode != NULL) {
+        Tail = Tail->NextNode;
+    }
+    Tail->NextNode = NewNode;
+    NewNode->PrevNode = Tail;
+    NewNode->NextNode = *Head;
+}
+
+Node* CreateNode(int Data) {
+    Node* NewNode = (Node*)malloc(sizeof(Node));
+    NewNode->Data = Data;
+    NewNode->NextNode = NULL;
+    NewNode->PrevNode = NULL;
+
+    return NewNode;    
+}
+
+int main() {
+    Node* List = NULL;
+
+    AppendNode(&List, CreateNode(10));
+    AppendNode(&List, CreateNode(100));
+
+    printf("%d\n", List->NextNode->NextNode->NextNode->Data);
+
+    return 0;
+}
