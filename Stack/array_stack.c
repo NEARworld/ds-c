17e2f8aedcafa4655ca3a931c1cbd55dd5be32e4
nearworld <artemismars2@outlook.com>
2023-12-28 22:23:10 +0900

    feat: CreateStack function

--- Stack/array_stack.c
+++ Stack/array_stack.c
@@ -1,14 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct tagNode {
     int Data;
 } Node;
 
-typedef struct arrayStack {
+typedef struct tagStack {
     int Capacity;
     int Top;
     Node* Nodes;
-} Stack;
+} ArrayStack;
+
+void CreateStack(ArrayStack** Stack, int Capacity) {
+    *Stack = (Node*)malloc(sizeof(ArrayStack));
+    (*Stack)->Nodes = (Node*)malloc(sizeof(Node) * Capacity); 
+
+    (*Stack)->Top = -1;
+    (*Stack)->Capacity = Capacity;
+}
 
 int main() {
+    ArrayStack* Stack = NULL;
+
+    CreateStack(&Stack, 5);
+
+    printf("top: %d, capacity: %d\n", Stack->Top ,Stack->Capacity);
 
     return 0;
 }
